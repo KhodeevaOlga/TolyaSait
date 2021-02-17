26c6ebe664a831bf363f2c55af2f8cf540ade5fc
KhodeevaOlga <khodeeva@icloud.com>
2021-02-17 12:37:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import './App.css';
 import React from 'react';
-import RouterTest from "./components/RouterTest/RouterTest";
-import {createBrowserHistory} from "history";
-
-const history = createBrowserHistory();
+import {useState} from 'react';
+import Menushka from './components/Menushka/Menushka';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Editorial from './pages/Editorial'
+import ClientShooting from "./pages/ClientShooting";
+import ModelTests from "./pages/ModelTests"
 
 function App() {
+    const [menuActive, setMenuActive] = useState(false)
+    const items = [{value: 'Editorial', href: '/editorial'}, {
+        value: 'Client Shooting',
+        href: '/clientShooting'
+    }, {value: 'Model tests', href: '/modelTests'}]
+
     return (
-        <RouterTest history={history}/>
-    )
-}
+
+        <div className='app'>
+            {/*<div className='App-header'>*/}
+            <nav>
+                <div
+                    className='burger-btn'
+                    onClick={() => setMenuActive(!menuActive)}>
+                </div>
+            </nav>
+
+
+            <main></main>
+
+
+        {/*компонента Менюшка*/}
+        <Menushka active={menuActive} setActive={setMenuActive} header={''} items={items}/>
+
+    <Router>
+        <Switch>
+            <Route path='/editorial' component={Editorial}/>
+            <Route path='/clientShooting' component={ClientShooting}/>
+            <Route path='/modelTests' component={ModelTests}/>
+            <div>
+            </div>
+
+        </Switch>
+    </Router>
+</div>
+)}
 
 export default App;
--- src/components/Menushka/Menushka.jsx
+++ src/components/Menushka/Menushka.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import './Menushka.css';
 
 const Menushka = ({header, items, active, setActive}) => {
-    return (
-        <div className={active ? 'menushka active' : 'menushka'} onClick={() => setActive(false)}>
-            <div className='blur'/>
-            {/*Благодаря пропагинейшену, меню закроется при нажатии вне поля контента менюхи*/}
-            <div className='menu_content' onClick={e => e.stopPropagation()}>
+
+
+        return (
+
+            <div className={active ? 'menushka active' : 'menushka'} onClick={() => setActive(false)}>
+                <div className='blur'/>
+
+                {/*Благодаря пропагинейшену, меню закроется при нажатии вне поля контента менюхи*/}
+                <div className='menu_content' onClick={e => e.stopPropagation()}>
                 <div className='menu_header'>{header}</div>
-                <ul>
-                    {items.map(item =>
-                        <li>
-                            <a href={item.href}>{item.value}</a>
-                        </li>
-                    )}
-                </ul>
+              <ul>
+                  {items.map(item =>
+                  <li>
+                      <a href={item.href}>{item.value}</a>
+                  </li>
+                  )}
+              </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 
 export default Menushka;
